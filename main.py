d15d3975576b39a69dd14d7e71b837c5a08da3fe
Hoyoshik <74919234+Hoyoshik@users.noreply.github.com>
2021-04-25 02:58:56 +0300

    Регистрация внешне

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for, request
+from data import db_session
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def showSignUp():
+    return render_template('first.html')
+
+@app.route('/main')
+def showSignUpi():
+    return render_template('index.html')
+
+@app.route('/register')
+def news():
+    return render_template('regist.html')
+
+@app.route('/characters')
+def characters():
+    return render_template('Hero.html')
+
+@app.route('/news')
+def News():
+    return render_template('new.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
